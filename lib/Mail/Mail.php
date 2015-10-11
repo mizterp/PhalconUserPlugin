d1c13caf389537a3cdcf2ef79314f7fa08ca6217
cp <carl.p@skillsrl.biz>
2015-10-11 15:05:12 -0300

    Optional use of mandrill api for sending html+text emails

--- lib/Mail/Mail.php
+++ lib/Mail/Mail.php
@@ -85,56 +85,75 @@ class Mail extends Component
      */
     public function send($to, $subject, $name = null, $params = null, $body = null)
     {
-        // Create the message
-        $message = \Swift_Message::newInstance();
+        $mailer = trim($this->config->pup->default->mailer) ?: 'swiftmailer';
 
         //Settings
         $mailSettings = $this->config->mail;
 
-        //Images
-        if (isset($params['images'])) {
-            $this->images = $params['images'];
-        }
+        if(strtolower($mailer) == 'mandrill' && $this->getDI()->get('mandrill')){
 
-        if (null === $body) {
-            $template = $this->getTemplate($message, $name, $params);
-        } else {
-            $template = $this->insertImages($message, $body);
-        }
-
-         // Setting message params
-        $message->setSubject($subject)
-            ->setTo($to)
-            ->setFrom(array(
-                $mailSettings->fromEmail => $mailSettings->fromName,
-            ))
-            ->setBody($template, 'text/html');
-
-        // Check attachments to add
-        foreach ($this->attachments as $file) {
-            $message->attach(\Swift_Attachment::newInstance()
-                ->setBody($file['content'])
-                ->setFilename($file['name'])
-                ->setContentType($file['type'])
+            $html = $this->getTemplate(null, $name, $params);
+            $text = $this->getTemplate(null, "{$name}_text", $params);
+            $to_array = array();
+            foreach($to as $to_email => $to_name ){
+                $to_array[] = array(
+                    'email' => $to_email,
+                    'name' => $to_name,
+                    'type' => 'to'
+                );
+            }
+            $email = array(
+                'html'      => $html,
+                'text'       => $text,
+                'subject'   => $subject,
+                'from_email' => $mailSettings->fromEmail,
+                'from_name' => $mailSettings->fromName,
+                'to' => $to_array
             );
+            $mandrill_api_settings = $this->config->mandrill_api_settings ? (array)$this->config->mandrill_api_settings : null;
+            if($mandrill_api_settings) $email = array_merge($email, $mandrill_api_settings);
+
+            $result = $this->getDI()->get('mandrill')->messages_send($email);
+
+            return $result;
+
+        }else{
+
+            // Create the message
+            $message = \Swift_Message::newInstance();
+
+            //Images
+            if(isset($params['images'])){
+                $this->images = $params['images'];
+            }
+
+            if(null === $body){
+                $template = $this->getTemplate($message, $name, $params);
+            }else{
+                $template = $this->insertImages($message, $body);
+            }
+
+            // Setting message params
+            $message->setSubject($subject)->setTo($to)->setFrom(array(
+                                                                    $mailSettings->fromEmail => $mailSettings->fromName,
+                                                                ))->setBody($template, 'text/html');
+
+            // Check attachments to add
+            foreach($this->attachments as $file){
+                $message->attach(\Swift_Attachment::newInstance()->setBody($file['content'])->setFilename($file['name'])->setContentType($file['type']));
+            }
+
+            if(!$this->_transport){
+                $this->_transport = \Swift_SmtpTransport::newInstance($mailSettings->smtp->server, $mailSettings->smtp->port, $mailSettings->smtp->security)->setUsername($mailSettings->smtp->username)->setPassword($mailSettings->smtp->password);
+            }
+
+            // Create the Mailer using your created Transport
+            $mailer = \Swift_Mailer::newInstance($this->_transport);
+            $result = $mailer->send($message);
+
+            $this->attachments = array();
+
+            return $result;
         }
-
-        if (!$this->_transport) {
-            $this->_transport = \Swift_SmtpTransport::newInstance(
-                $mailSettings->smtp->server,
-                $mailSettings->smtp->port,
-                $mailSettings->smtp->security
-            )
-            ->setUsername($mailSettings->smtp->username)
-            ->setPassword($mailSettings->smtp->password);
-        }
-
-        // Create the Mailer using your created Transport
-        $mailer = \Swift_Mailer::newInstance($this->_transport);
-        $result = $mailer->send($message);
-
-        $this->attachments = array();
-
-        return $result;
     }
 }
